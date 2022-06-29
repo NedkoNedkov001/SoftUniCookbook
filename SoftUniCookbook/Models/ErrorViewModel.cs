fdacd89f365fdea1b1548d441ecdc60ff1b4bd2e
Nedko Nedkov <nedkonedkov001@gmail.com>
2022-06-29 13:11:46 +0200

    Add project files.

---
+++ SoftUniCookbook/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SoftUniCookbook.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
