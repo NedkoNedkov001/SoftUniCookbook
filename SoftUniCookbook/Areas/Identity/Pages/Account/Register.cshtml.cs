57fb0c5bea5f8786eb82ec406ac71d3cf507eac3
Nedko Nedkov <nedkonedkov001@gmail.com>
2022-07-07 16:51:35 +0200

    Added usernames

--- SoftUniCookbook/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SoftUniCookbook/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -70,6 +70,11 @@ namespace Cookbook.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            [Required]
+            [StringLength(25, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+            [Display(Name = "Username")]
+            public string Username { get; set; }
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -113,11 +118,11 @@ namespace Cookbook.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
-
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                
+                await _userStore.SetUserNameAsync(user, Input.Username, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
-
+                
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
