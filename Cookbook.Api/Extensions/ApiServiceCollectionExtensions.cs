c86a110f1944723be6b64a369162ce4d83ea0563
Nedko Nedkov <nedkonedkov001@gmail.com>
2022-07-13 15:13:34 +0200

    Added web api

---
+++ Cookbook.Api/Extensions/ApiServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Cookbook.Infrastructure.Data;
+using Cookbook.Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ApiServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        {
+            services.AddScoped<IApplicationDbRepository, ApplicationDbRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApiDbContexts(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            return services;
+        }
+
+    }
+}
