9ec0a28077a704480ca5f1eff8a811c437860af5
Nedko Nedkov <nedkonedkov001@gmail.com>
2022-07-17 13:19:00 +0200

    Added user service

---
+++ SoftUniCookbook.Core/Contracts/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using Cookbook.Core.Models;
+using Cookbook.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cookbook.Core.Contracts
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<UserListViewModel>> GetUsers();
+    }
+}
---
+++ SoftUniCookbook.Core/Services/UserService.cs
@@ -0,0 +1,37 @@
+﻿using Cookbook.Core.Contracts;
+using Cookbook.Core.Models;
+using Cookbook.Infrastructure.Data.Models;
+using Cookbook.Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cookbook.Core.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IApplicationDbRepository repo;
+
+        public UserService(IApplicationDbRepository repo)
+        {
+            this.repo = repo;
+        }
+    
+        public async Task<IEnumerable<UserListViewModel>> GetUsers()
+        {
+            return await repo.All<ApplicationUser>()
+                .Select(u => new UserListViewModel()
+                {
+                    Id = u.Id,
+                    Username = u.UserName,
+                    Email = u.Email,
+                    PhoneNumber = u.PhoneNumber,
+                    Picture = u.Picture
+                })
+                .ToListAsync();
+        }
+    }
+}
