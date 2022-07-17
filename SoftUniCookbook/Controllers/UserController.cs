f56f7faf7e4c90ae797a6d7b4fc6291814482f7d
Nedko Nedkov <nedkonedkov001@gmail.com>
2022-07-17 13:20:17 +0200

    Added user controller

---
+++ SoftUniCookbook/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Cookbook.Core.Constants;
+using Cookbook.Core.Contracts;
+using Cookbook.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cookbook.Controllers
+{
+    public class UserController : BaseController
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly IUserService userService;
+
+        public UserController(RoleManager<IdentityRole> roleManager,
+            UserManager<ApplicationUser> userManager,
+            IUserService userService)
+        {
+            this.roleManager = roleManager;
+            this.userManager = userManager;
+            this.userService = userService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
