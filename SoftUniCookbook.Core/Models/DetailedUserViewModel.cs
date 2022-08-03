a75906c68c3b75d748179f681bdf2e13b41a128f
Nedko Nedkov <nedkonedkov001@gmail.com>
2022-08-03 17:33:37 +0200

    Added DetailedUserViewModel

---
+++ SoftUniCookbook.Core/Models/DetailedUserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cookbook.Core.Models
+{
+    public class DetailedUserViewModel
+    {
+        public string Id { get; set; }
+        public byte[] Picture { get; set; }
+        public IFormFile NewPicture { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string About { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+}
