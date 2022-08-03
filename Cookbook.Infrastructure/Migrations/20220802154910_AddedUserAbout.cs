39bb2763475a1f82e6edf9bfdb2fd2717d5ed12a
Nedko Nedkov <nedkonedkov001@gmail.com>
2022-08-03 17:29:52 +0200

    Added user about me table

---
+++ Cookbook.Infrastructure/Migrations/20220802154910_AddedUserAbout.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SoftUniCookbook.Infrastructure.Migrations
+{
+    public partial class AddedUserAbout : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "About",
+                table: "AspNetUsers",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "About",
+                table: "AspNetUsers");
+        }
+    }
+}
